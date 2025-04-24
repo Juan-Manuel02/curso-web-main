a3918be25197969604d1acda97a1754dcce88916
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-04-24 16:14:18 +0200

    ejercicios html css y js

---
+++ ejercicio_repaso_03_GRID/01.js
@@ -0,0 +1,50 @@
+/* const a = 10;
+const b = 0;
+
+// Objetivo: 100
+console.log (String (a) + b); // 100
+console.log (Number (String (a) + b)); // 100
+console.log (typeof (String (a) + b)); // 100
+console.log (a*a); // 100  <-- otra opción
+
+// objetivo: "010010"
+console.log (String (a).padStart (3,0) + String (a).padStart (3,0)); // 010010
+console.log (String (b) + String (a) + String (b) + String (a)); // 010010
+console.log (String (b) + String (a) + a + b);
+console.log (String (b) + a + b + a); // 010010 */
+
+/* ronda 2
+const a = true;
+const b = "2";
+console.log (Number (a) + Number (b)); // 3 */
+
+/* ronda 3
+const a = 2;
+const b = "3";
+// objetivo: "true6"
+console.log (String (Boolean (a)) + a * b);
+console.log (Boolean (a), a * b); */
+
+// ronda 4
+const a = 2;
+const b = 3;
+const c = 100;
+const d = 10;
+const e = 0;
+// Objetivo: 550
+console.log (Number (a + b) * (c + d));
+
+
+/* Esto es una putada
+console.log (3 + 3); // 6
+console.log ("3" + "3") // 33
+console.log ("3" + +3); // 33
+console.log (3 + "3"); // 6 number
+console.log (3 + 3 + -3); // 3 number
+console.log ("3" + "3" + - "3"); // 30 number */
+
+
+
+
+
+  
