37c9dc6c1534071fbd115e087861e7579c48735c
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-05-09 18:13:25 +0200

    Ejercicio objeto optimizado js

--- repasoJS_sesion3/02_1_objeto_optimizado.js
+++ repasoJS_sesion3/02_1_objeto_optimizado.js
@@ -7,7 +7,7 @@ Por último, crea un método estado() que muestre el modelo del auto y su nivel 
 Realiza una serie de llamadas a los métodos para probar su funcionamiento.
 */
 
-const MAX_COMBUSTIBLE = 50;
+ /*const MAX_COMBUSTIBLE = 50;
 
 const auto = {
   modelo: "Toyota Corolla",
@@ -69,4 +69,75 @@ auto.estado();        // 0 litros
 auto.repostar(30);
 auto.estado();        // 30 litros
 auto.repostar(25);    // se llena hasta 50, no más
-auto.estado();        // 50 litros
+auto.estado();        // 50 litros */
+
+// Otra forma de hacer este ejercicio
+const validarNumero = valor => typeof valor === 'number';
+const validarPositivo = valor => valor > 0;
+const calcularConsumo = km => km * 0.2;
+
+const MAX_COMBUSTIBLE = 50;
+
+const coche = {
+  modelo: "Toyota Carolla",
+  combustible: 50,
+
+  conducir (km){
+    if (!validarNumero (km)) {
+      console.log("Error: los km tienen que ser un número.");
+      return;
+    }
+    if (!validarPositivo (km)) {
+      console.log("Error: los km no pueden ser negativos.");
+      return;
+    }
+    if (this.combustible === 0) {
+      console.log(`${this.modelo} no puede conducir: sin combustible.`);
+      return;
+    }
+    const consumo = calcularConsumo(km);
+    if (this.combustible === 0) {
+      console.log(`${this.modelo} no puede conducir ${km}: sin combustible.`);
+      return;
+    }
+    this.combustible -= consumo;
+    console.log(`${this.modelo} condujo ${km} km. Combustible restante: ${this.combustible.toFixed(1)} litros.`);
+  },
+
+  repostar (litros) {
+    if (!validarNumero (litros)) {
+      console.log("Error: los litros tienen que ser un número.");
+      return;
+    }
+    if (litros <= 0) {
+      console.log("Error: Los litros deben ser mayores que 0.");
+      return;
+    }
+    if (this.combustible === 50) {
+      console.log(`${this.modelo} ya tiene el tanque lleno.`);
+      return;
+    }
+    const combustibleAntes = this.combustible
+    this.combustible += litros;
+
+    if (this.combustible > 50) {
+      this.combustible = 50;
+    }
+    const repostado = this.combustible - combustibleAntes;
+    console.log(`${this.modelo} repostó ${repostado.toFixed(1)} litros. Combustible total: ${this.combustible.toFixed(1)} litros.`);
+  },
+
+  estado () {
+    console.log(`${this.modelo} tiene ${this.combustible.toFixed(1)} litros de combustible`);
+  }
+}
+
+// Prueba final
+coche.conducir(100);
+coche.estado();
+coche.conducir(200);
+coche.estado();
+coche.repostar(30);
+coche.estado();
+coche.repostar(25);
+coche.estado();
