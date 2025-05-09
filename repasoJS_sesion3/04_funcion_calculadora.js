1ad47c0b06458a239c3c2058052cc0b3649193f6
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-05-09 16:16:14 +0200

    Nuevos ejercicios de programación js

--- repasoJS_sesion3/04_funcion_calculadora.js
+++ repasoJS_sesion3/04_funcion_calculadora.js
@@ -6,3 +6,44 @@ Operación será un string con el símbolo de la operación: "+", "-", "*", "/".
 En la division, si el segundo número es 0, la función debe devolver "Error: División por cero".
 Realiza una serie de llamadas a la función calculadora para probar su funcionamiento.
 */
+
+// Funciones para operaciones matemáticas
+function sumar (a, b) {
+    return a + b;
+}
+function restar (a, b) {
+    return a - b;
+}
+function multiplicar (a, b) {
+    return a * b;
+}
+function dividiir (a, b) {
+    if (b === 0) {
+        return "Error: División por cero";
+    }
+    return a / b;
+}
+
+// Función calculadora
+function calculadora (operacion, a, b) {
+    switch (operacion) {
+        case "+":
+            return sumar(a, b);
+        case "-":
+            return restar(a, b);
+        case "*":
+            return multiplicar(a, b);
+        case "/":
+            return dividiir(a, b);
+        default:
+            return "Operación no válida";
+    }
+}
+
+// Pruebas
+console.log(calculadora("+", 5, 3)); 
+console.log(calculadora("-", 5, 3)); 
+console.log(calculadora("*", 5, 3));
+console.log(calculadora("/", 5, 3));
+console.log(calculadora("/", 5, 0));
+
